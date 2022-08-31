101e9785014c33fc4e701b8616a1b183c3f8e665
acastells <36441239+acastells@users.noreply.github.com>
2022-08-31 12:44:58 +0200

    imported animal models

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 3;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
+    Animator anim;
+    Rigidbody rb;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(0f, 0.0f, 1f);
+        anim.SetInteger("Walk", 1);
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void Jump()
+    {
+        if (Time.time > canJump)
+        {
+            rb.AddForce(0, jumpForce, 0);
+            canJump = Time.time + timeBeforeNextJump;
+            anim.SetTrigger("jump");
+        }
+    }
+}
